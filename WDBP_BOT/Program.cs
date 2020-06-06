6d720aca723eb0a7d4a1137db7b3683f50469492
towatti1009 <64978137+towatti1009@users.noreply.github.com>
2020-06-06 10:45:10 +0900

    BOT機能追加準備

---
+++ WDBP_BOT/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Discord;
+
+namespace WDBP_BOT
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        public static CommandService _commands;
+        public static IServiceProvider _services;
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+            _client.Log += Log;
+            _commands = new CommandService();
+            _services = new ServiceCollection().BuildServiceProvider();
+            _client.MessageReceived += CommandRecieved;
+            //次の行に書かれているstring token = "hoge"に先程取得したDiscordTokenを指定する。
+            string token = "NzE4NjE2NjExODgzMzE5MzU4.XtrfGQ.E2_qRYaGxlcwTc2f2pma5hHsn4s";
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        /// <summary>
+        /// 何かしらのメッセージの受信
+        /// </summary>
+        /// <param name="msgParam"></param>
+        /// <returns></returns>
+        private async Task CommandRecieved(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+
+            //デバッグ用メッセージを出力
+            Console.WriteLine("{0} {1}:{2}", message.Channel.Name, message.Author.Username, message);
+            //メッセージがnullの場合
+            if (message == null)
+                return;
+
+            //発言者がBotの場合無視する
+            if (message.Author.IsBot)
+                return;
+
+
+            var context = new CommandContext(_client, message);
+
+            //ここから記述--------------------------------------------------------------------------
+            var CommandContext = message.Content;
+
+            // コマンド("おはよう")かどうか判定
+            if (CommandContext == "おはよう")
+            {
+                await message.Channel.SendMessageAsync("Hello!");
+            }
+
+
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
